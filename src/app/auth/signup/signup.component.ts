89a2aa5f76583d05911f1720e0478da42bb798f1
Abnumber3 <114044297+Abnumber3@users.noreply.github.com>
2026-06-20 23:41:44 -0400

    forms challenge

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,9 +1,37 @@
 import { Component } from '@angular/core';
+import { ReactiveFormsModule, Validators, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
   standalone: true,
+  imports: [ReactiveFormsModule],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.css',
 })
-export class SignupComponent {}
+export class SignupComponent {
+
+  signupForm = this.fb.group({
+    email: ['', [
+      Validators.required,
+      Validators.email
+    ]],
+
+    password: ['', [
+      Validators.required,
+      Validators.minLength(6)
+    ]]
+  })
+
+
+  constructor(private fb: FormBuilder){} 
+
+
+  signUp(){
+
+  }
+
+  resetForm(){
+
+  }
+  
+}
