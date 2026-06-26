ae2358b3baed15e53a87bd42e8e3dc0de7d5901c
Abnumber3 <114044297+Abnumber3@users.noreply.github.com>
2026-06-25 22:00:29 -0400

    forms practice checkboxes arrays

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { ReactiveFormsModule, Validators, FormBuilder } from '@angular/forms';
+import { ReactiveFormsModule, Validators, FormBuilder, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
@@ -10,20 +10,76 @@ import { ReactiveFormsModule, Validators, FormBuilder } from '@angular/forms';
 })
 export class SignupComponent {
 
+
+  findUsOptions = ['Google', 'Referred by friend', 'Other']
+
+
+
   signupForm = this.fb.group({
     email: ['', [
       Validators.required,
       Validators.email
     ]],
-
     password: ['', [
       Validators.required,
-      Validators.minLength(5)
+      Validators.minLength(7)
+    ]],
+    confirmPassword:['',[
+      Validators.required,
+      Validators.minLength(7)
+    ]],
+
+
+    //Mailing info Subgroup
+
+    mailingInfo: this.fb.group({
+
+    firstName: ['', [
+      Validators.required
+    ]],
+    lastName: ['', [
+      Validators.required
+    ]],
+
+    street: ['', [
+      Validators.required
+    ]],
+    number: ['', [
+      Validators.required
+    ]],
+    postalCode: ['', [
+      Validators.required
+    ]],
+    city: ['', [
+      Validators.required
     ]]
+    }),
+
+
+
+
+    //Acquisition info
+
+
+    acquisition: this.fb.group({
+
+     findUs: this.fb.array(this.findUsOptions.map(()=>{
+        return this.fb.control(false);
+      })),
+
+      
+      role: new FormControl<'student' | 'teacher' | 'employee' | 'founder' | 'other'>('student',  [
+    ]),
+
+    agreedToTerms: [false, [Validators.requiredTrue]]
+  })
+
   })
 
 
-  constructor(private fb: FormBuilder){} 
+  constructor(
+    private fb: FormBuilder,
+  ){} 
 
 
 
@@ -47,6 +103,12 @@ export class SignupComponent {
 
 
   signUp(){
+     const rawCheckBoxes = this.signupForm.value.acquisition?.findUs as boolean[] | undefined;
+
+    //  const findUsSelection = rawCheckBoxes
+    //    ?.map((checked, index) => checked ? this.findUsOptions[index] : null)
+    //    .filter((value): value is string => !!value);
+
      console.log(this.signupForm.value);
   }
 
